f385e67f9281fc5cfca1b4b6b46807d4639b7b8e
fourthstanza <kitkatmatthew@hotmail.com>
2026-02-17 16:22:49 -0500

    added new model using jibril's dataset (in data lake)

--- deployment/deployment/deployment.ino
+++ deployment/deployment/deployment.ino
@@ -10,16 +10,15 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-#define ANALOG_IN_PIN  15 // ESP32 pin GPIO36 (ADC0) connected to voltage sensor
+#define ANALOG_IN_PIN  15 // ESP32 pin GPIO15 (ADC1) connected to sensor
 #define REF_VOLTAGE    1.3
 #define ADC_RESOLUTION 4095.0
 #define R1             30000.0 // resistor values in voltage sensor (in ohms)
 #define R2             7500.0  // resistor values in voltage sensor (in ohms)
+#define PUSHBUTTON     36 // GPIO pin of pushbutton
 
-int pushButton = 36;
-
-  int xpos = 5;
-  int ypos = 40;
+int xpos = 5;
+int ypos = 40;
 
 void setup() {
   Serial.begin(9600);
@@ -31,9 +30,9 @@ void setup() {
 
   Serial.println("Screen initialized.");
 
-  analogSetAttenuation(ADC_6db); // for the voltage at min resistance of the pot
+  analogSetAttenuation(ADC_6db); // for the voltage at max resistance of the pot
 
-  pinMode(pushButton, INPUT);
+  pinMode(PUSHBUTTON, INPUT);
 
   drawtext("Device initialized.\nBeginning run.", TFT_WHITE, 0, ypos);
   Serial.println("Device initialized.\nBeginning run.");
@@ -43,8 +42,8 @@ void setup() {
 }
 
 void loop() {
-  float voltage = 0;
-  int buttonState = digitalRead(pushButton);
+  float voltage = readADC();
+  int buttonState = digitalRead(PUSHBUTTON);
 
   if (buttonState == 1){
     int NUM_SAMPLES = 100;
@@ -66,8 +65,7 @@ void loop() {
     Serial.println("]");
   }
 
-  voltage = readADC();
-  //triangles(voltage);
+  
   tft.fillScreen(TFT_BLACK);
   tft.drawFloat(voltage, 2, xpos, ypos);
   delay(100);
@@ -84,22 +82,4 @@ void drawtext(char *text, uint16_t color, int xp, int yp){
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.print(text);
-}
-
-void triangles() {
-  tft.fillScreen(TFT_BLACK);
-  int color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t+=1) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-    delay(100);
-  }
 }
