565be63bba6deec9b664411c69269aa179ac2c66
fourthstanza <kitkatmatthew@hotmail.com>
2026-02-20 13:33:37 -0500

    functionalized parquet to pandas converter

--- data_generator/parquetToPd.py
+++ data_generator/parquetToPd.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
+# Converts Apache Parquet files into pandas dataframes.
+
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR.joinpath('data')
 
-# Path to your parquet file
-file_path = DATA_DIR.joinpath("ethylene_methane_ds_10hz.parquet")
- 
-# Load dataset
-df = pd.read_parquet(file_path)
-print(df)
+def parquetToDf(name): 
+    file_path = DATA_DIR.joinpath(str(name))
+    df = pd.read_parquet(file_path)
+    return df
