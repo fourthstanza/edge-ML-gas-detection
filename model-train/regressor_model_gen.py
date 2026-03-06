0cf7d474bb26eb04180089a6e1e320b297cba0f0
fourthstanza <kitkatmatthew@hotmail.com>
2026-03-06 18:26:19 -0500

    added validation set in split, bugfixing

--- model-train/regressor_model_gen.py
+++ model-train/regressor_model_gen.py
@@ -12,7 +12,7 @@ CSV: bool = False
 
 INPUTS: int = 16                                       # Number of features to be used for training the model
 X_COLUMNS: list = list(range(3, 3+INPUTS))             # Column(s) to be used as features 
-Y_COLUMNS: list = [1]                                  # Column to be used as the target variable 
+Y_COLUMNS: list | int = 1                              # Column(s) to be used as the target variable. Only use a list if there are multiple target variables.
 T_COLUMN:  int = 0                                     # Column representing time (in seconds)
 FREQUENCY: int = 10                                    # Sampling frequency of the dataset (in Hz)
 
@@ -23,9 +23,7 @@ WINDOW_SIZE: int = 200                                 # Size of the sliding win
 # ------------------------------------------------------ #
 
 import numpy as np
-import pandas as pd
 import random
-import os
 import tensorflow as tf # Use Keras API from TensorFlow
 
 from pathlib import Path
@@ -46,10 +44,11 @@ class DataSplit:
 
     tr_ind: list[int]
     tst_ind: list[int]
+    val_ind: list[int]
 
     winlen: int
 
-    def __init__(self, X: np.ndarray, y:np.ndarray, win_length: int, split:  float | tuple[float, float] = 0.8, time: np.ndarray | None = None, seed: int | None = None):
+    def __init__(self, X: np.ndarray, y:np.ndarray, win_length: int, split: tuple[float, float, float] | None, time: np.ndarray | None = None, seed: int | None = None):
 
         if win_length > X.shape[0]:
             raise ValueError("Length of window must be smaller than length of dataset")
@@ -62,12 +61,19 @@ class DataSplit:
 
         self.data_length = self.features.shape[0]
 
-        if isinstance(split, tuple):
-            split = split[0]
+        if split is not None:
+            if abs(sum(split) - 1.0) > 1e-6:
+                raise ValueError("Split proportions must sum to 1")
+            if split[0] <= 0 or split[1] <= 0 or split[2] <= 0:
+                raise ValueError("Split proportions must be positive")
+            if split[1] * self.data_length-win_length < 1 or split[2] * self.data_length-win_length < 1:
+                raise ValueError("Split proportions too small, no data allocated to test or validation set")
+        else:
+            split  = (0.6, 0.2, 0.2)
 
         self.split(split, seed)
 
-    def split(self, split: float, seed: int | None = None):
+    def split(self, split: tuple[float, float, float], seed: int | None = None):
         
         if seed is None:
             seed = random.randint(0, 2 ** 32)
@@ -78,15 +84,23 @@ class DataSplit:
         datarange = list(range(self.winlen,self.data_length))
 
         # choose k random indices w/o replacement for training
-        self.tr_ind = random.sample(datarange, k = int(len(datarange) * split))
-        # remaining indices are for testing
-        self.tst_ind = list(set(datarange) - set(self.tr_ind))
+        self.tr_ind = random.sample(datarange, k = int(len(datarange) * split[0]))
+        # remaining indices are for testing & validation
+        remaining = list(set(datarange) - set(self.tr_ind))
+
+        # proportion of remaining data to be used for testing vs validation
+        prop = split[1] / (split[1] + split[2]) 
+        self.tst_ind = random.sample(remaining, k = int(len(remaining) * prop))
+        self.val_ind = list(set(remaining) - set(self.tst_ind))
 
     def tr_length(self) -> int:
         return len(self.tr_ind)
     
     def tst_length(self) -> int:
         return len(self.tst_ind)
+
+    def val_length(self) -> int:
+        return len(self.val_ind)
 
     def Xtr_i(self, i: int) -> np.ndarray | float:
         ind = self.tr_ind[i]
@@ -116,6 +130,20 @@ class DataSplit:
         else:
             return self.target[ind, :]
 
+    def Xval_i(self, i: int) -> np.ndarray | float:
+        ind = self.val_ind[i]
+        if len(self.features.shape) == 1:
+            return self.features[ind-self.winlen:ind]
+        else:
+            return self.features[ind-self.winlen:ind, :]
+        
+    def yval_i(self, i: int) -> np.ndarray | float:
+        ind = self.val_ind[i]
+        if len(self.target.shape) == 1:
+            return self.target[ind]
+        else:
+            return self.target[ind, :]
+
     def ttr_i(self, i: int) -> float | None:
         if self.time is None:
             raise ValueError("No time data provided to DataSplit, cannot get time value")
@@ -127,8 +155,14 @@ class DataSplit:
             raise ValueError("No time data provided to DataSplit, cannot get time value")
         ind = self.tst_ind[i]
         return self.time[ind]
+    
+    def tval_i(self, i: int) -> float | None:
+        if self.time is None:
+            raise ValueError("No time data provided to DataSplit, cannot get time value")
+        ind = self.val_ind[i]
+        return self.time[ind]
 
-def create_data_split(data: np.ndarray, split: float | tuple[float, float], seed: int | None = None) -> DataSplit:
+def create_data_split(data: np.ndarray, split: tuple[float, float, float] | None = None, seed: int | None = None) -> DataSplit:
 
     winlength = WINDOW_SIZE * FREQUENCY
 
@@ -136,7 +170,7 @@ def create_data_split(data: np.ndarray, split: float | tuple[float, float], seed
     y = data[:, Y_COLUMNS]
     t = data[:, T_COLUMN]
 
-    splt = DataSplit(X, y, winlength, split, time = t, seed = seed)
+    splt = DataSplit(X, y, winlength, split=split, time = t, seed = seed)
 
     return splt
 
@@ -151,7 +185,7 @@ def main():
 
     data = df.to_numpy()
 
-    data_split = create_data_split(data, split = 0.8, seed = 42)
+    data_split = create_data_split(data, seed = 42)
 
     
 
