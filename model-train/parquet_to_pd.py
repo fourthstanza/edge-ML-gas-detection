0af275098ceef2e3b8066260ed8d5f1b7d3c47cb
fourthstanza <kitkatmatthew@hotmail.com>
2026-03-06 17:43:25 -0500

    renamed files

---
+++ model-train/parquet_to_pd.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# Converts Apache Parquet files into pandas dataframes.
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = ROOT_DIR.joinpath('generated-data')
+
+def parquetToDf(name, data_dir=DATA_DIR, columns: list[str] | None = None) -> pd.DataFrame: 
+    file_path = data_dir.joinpath(str(name))
+    df = pd.read_parquet(file_path, columns = columns)
+    return df
